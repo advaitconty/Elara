b0102ded3a6c6524a6c893dd0091136b0d0c3d55
AdvaitThePro <advaitthepro@outlook.com>
2024-12-28 17:19:06 +0800

    Initial Commit

---
+++ Solstice/SolsticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolsticeApp.swift
+//  Solstice
+//
+//  Created by Milind Contractor on 28/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SolsticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
