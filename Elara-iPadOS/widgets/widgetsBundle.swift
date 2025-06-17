6f0d6074fee2541c76efb11a88a370a13a68f176
contyadvait <advaitthepro@outlook.com>
2025-06-17 10:40:21 +0800

    added widgets

---
+++ Elara-iPadOS/widgets/widgetsBundle.swift
@@ -0,0 +1,17 @@
+//
+//  widgetsBundle.swift
+//  widgets
+//
+//  Created by Milind Contractor on 17/6/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widgetsBundle: WidgetBundle {
+    var body: some Widget {
+        widgets()
+        widgetsLiveActivity()
+    }
+}
